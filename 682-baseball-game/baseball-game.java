d70a07085a7e0fa8139a3233277f7b3723e7edcc
Aryan Dasari <114567152+aryand1009@users.noreply.github.com>
2026-02-04 18:52:37 +0530

    Time: 3 ms (76.41%) | Memory: 43.3 MB (94.84%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,25 +1,54 @@
+// class Solution {
+//     public int calPoints(String[] operations) {
+
+//         Stack<Integer> stack = new Stack<>();
+
+//         for(String op : operations) {
+//             if(op.equals("C")) stack.pop();
+//             else if(op.equals("D")) stack.push(2 * stack.peek());
+//             else if(op.equals("+")) {
+//                 int top = stack.pop();
+//                 int newScore = top + stack.peek();
+//                 stack.push(top);
+//                 stack.push(newScore);
+//             }
+//             else stack.push(Integer.parseInt(op));
+//         }
+
+//         int sum = 0;
+//         for(int n : stack) {
+//             sum += n;
+//         }
+
+//         return sum;
+//     }
+// }
+
+
+
+
+
 class Solution {
     public int calPoints(String[] operations) {
-
-        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
 
         for(String op : operations) {
-            if(op.equals("C")) stack.pop();
-            else if(op.equals("D")) stack.push(2 * stack.peek());
-            else if(op.equals("+")) {
-                int top = stack.pop();
-                int newScore = top + stack.peek();
-                stack.push(top);
-                stack.push(newScore);
-            }
-            else stack.push(Integer.parseInt(op));
+            int n = list.size();
+
+            if(op.equals("C")) list.remove(n-1);
+
+            else if(op.equals("D")) list.add(2 * list.get(n-1));
+
+            else if(op.equals("+")) list.add(list.get(n-1) + list.get(n-2));
+
+            else list.add(Integer.parseInt(op));
         }
 
         int sum = 0;
-        for(int n : stack) {
-            sum += n;
+        for(int x : list) {
+            sum += x;
         }
-
+        
         return sum;
     }
 }
